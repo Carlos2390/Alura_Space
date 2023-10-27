3a8a68b75e361650db6b44bb585a47f26b928026
Carlos Matos <carlinhos12345678962@gmail.com>
2023-10-27 17:01:13 -0300

    aula2 curso2

--- setup/urls.py
+++ setup/urls.py
@@ -16,15 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from galeria.views import index, imagem
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', index),
-#     path('imagem/', imagem),
-# ]
 
 urlpatterns = [
-    path("/admin", admin.site.urls),
+    path("admin", admin.site.urls),
     path('', include('galeria.urls')),
 ]
